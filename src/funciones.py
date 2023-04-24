9b1e815ea6f33edc8712eb0649b16d5425dea094
Nerea Riveiro <nereaironhack@gmail.com>
2023-04-24 16:44:00 +0200

    e

--- src/funciones.py
+++ src/funciones.py
@@ -3,3 +3,14 @@ def nulitos(df):
     print(nul_col[nul_col>0])
 
 
+def modificar_nombre(nombre):
+    if nombre.upper() in 'USA':
+        return nombre
+    elif 'UAE' in  nombre.upper():
+        return 'UAE'
+    elif nombre.endswith('?'):
+        return nombre.title()[:-1]
+    elif nombre is 'N/A':
+        return 'N/A'
+    else:
+        return nombre.title()
