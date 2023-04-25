49402943b30118cefd64e8e7def708031f267a69
Nerea Riveiro <nereaironhack@gmail.com>
2023-04-25 03:37:48 +0200

    h

--- src/funciones.py
+++ src/funciones.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+
+
 def nulitos(df):
     nul_col = df.isna().sum()
     print(nul_col[nul_col>0])
@@ -17,3 +21,27 @@ def modificar_nombre(nombre):
         return nombre
     else:
         return nombre.title()
+    
+
+
+def completar_valores(row):
+    if pd.isnull(row['Area']):
+        row['Area'] = row['Location']
+    if pd.isnull(row['Area']) and pd.isnull(row['Location']):
+        row['Area'] = 'unknown'
+    if pd.isnull(row['Location']):
+        row['Location'] = 'unknown'
+    return row
+
+
+dict_activ = {"Board surfing" : ".*(surf).*|.*(boogie board).*|.*(body board).*",
+              "Kayaking & similar" : ".*(kayak).*|.*(canoe).*|.*(rowing).*",
+              "Diving" : ".*(diving).*",
+              "Paddle boarding" : ".*(paddle).*",
+              "Sailing" : ".*(boat).*|.*(sail).*|.*(ship).*|.*(overboard).*",
+              "Snorkeling" : ".*(snorkel).*",
+              "Swimming" : ".*(bathing).*|.*(swimming).*|.*(float).*",
+              "Spear-fishing" : ".*(spearfishing).*",
+              "Fishing" : "[\w\s]+?(fishing).*|^(fishing).*",
+              "Wading" : ".*wad.*|.*(walking).*|.*(standing).*|.*(treading).*"
+}
